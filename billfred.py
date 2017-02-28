39fb13823c08ffcc72c104711ec894be49751a36
Vladimir Gorbunov <vsg@suburban.me>
2017-03-01 00:24:03 +0300

    Add basic logging support

--- billfred.py
+++ billfred.py
@@ -8,6 +8,7 @@ import sys
 import os
 
 import logging
+import logging.config
 
 import sleekxmpp
 import configparser
@@ -30,9 +31,13 @@ else:
 #path + name
 db_full_path=""
 
+# Get billfred logger
+logger = logging.getLogger('billfred')
+
 
 def db_thread(queue):
     """Thread function that writes log to sqlite db."""
+    logger.debug('Opening database %s', db_full_path)
     db = sqlite3.connect(db_full_path)
     while True:
         query = queue.get()
@@ -40,20 +45,20 @@ def db_thread(queue):
         if query == 'stop':
             break
         cursor = db.cursor()
+        logger.debug('Writing %s to database', query)
         cursor.execute(
             'INSERT INTO chat_log (time, jit, name, message) VALUES (?,?,?,?)',
             query
         )
         cursor.close()
         db.commit()
+    logger.debug('Closing database %s', db_full_path)
     db.close()
 
 
 class BBot(sleekxmpp.ClientXMPP):
 
     def __init__(self, jid, password, room, nick, queue):
-
-        
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
 
         self.room = room
@@ -66,7 +71,6 @@ class BBot(sleekxmpp.ClientXMPP):
                                self.muc_online)
 
     def start(self, event):
-        
         self.get_roster()
         self.send_presence()
         self.plugin['xep_0045'].joinMUC(self.room,
@@ -74,7 +78,6 @@ class BBot(sleekxmpp.ClientXMPP):
                                         wait=True)
 
     def muc_message(self, msg):
-    
         # Cmopose data and write message to  chat log
         nick = msg['mucnick'] # User nick sowed in chat room
         jid = str(msg['from'])     # ful JID Like user@jabb.en/UserName
@@ -107,18 +110,20 @@ class BBot(sleekxmpp.ClientXMPP):
             #self.try_say_url_info(msg['body'], msg['from'])
 
     def try_ping(self, pingjid, nick):
+        """Ping user."""
+        logger.debug('Got ping from nick "%s" jid "%s"', nick, pingjid)
         try:
             rtt = self['xep_0199'].ping(pingjid,timeout=10)
             self.send_message(mto=pingjid.bare,
                                 mbody="%s, pong is: %s" % (nick, rtt),
                                 mtype='groupchat')
-                #logging.info("Success! RTT: %s", rtt)
+            logger.debug('Successfully pinged %s (%s)', nick, pingjid)
         except IqError as e:
-            logging.info("Error pinging %s: %s",
-                    pingjid,
-                    e.iq['error']['condition'])
+            logger.info("Error pinging %s: %s",
+                        pingjid,
+                        e.iq['error']['condition'])
         except IqTimeout:
-            logging.info("No response from %s", pingjid)
+            logger.info("No response from %s", pingjid)
 
 
     def muc_online(self, presence):
@@ -130,19 +135,19 @@ class BBot(sleekxmpp.ClientXMPP):
 
     def write_chat_log(self, query):
         self.queue.put(query)
-     
-  
 
-            
+
 if __name__ == '__main__':
     #load config file 
     config = configparser.ConfigParser()
     config.read('billfred.cfg')
 
     #Setup logging.
-    loglevel = config['log']['loglevel']
-    logging.basicConfig(level=loglevel,
-                        format='%(levelname)-8s %(message)s')
+    logging.config.fileConfig('billfred.cfg')
+
+    # loglevel = config['log']['loglevel']
+    # logging.basicConfig(level=loglevel,
+    #                     format='%(levelname)-8s %(message)s')
     
     #Database
     dbname = config['database']['database_name'] 
@@ -163,16 +168,15 @@ if __name__ == '__main__':
                             name TEXT NOT NULL,
                             message TEXT)""")
         connect_db.close()
-        print("warning: New chat log database was created")
+        logger.warning('New databse was created')
 
     #Connect to account
     jid = config['account']['jid'] 
     password = config['account']['password']
     room = config['account']['room']
     nick = config['account']['nick']
-    if (not jid) or (not password) or (not room):
-        #debug print('{:<5}| jid'.format(jid))
-        print("wrong account parametrs.  exit...")
+    if not any([jid, password, room]):
+        logger.error('Wrong account parameters, exiting')
         sys.exit(78)
 
     db_queue = queue.Queue()
@@ -192,9 +196,9 @@ if __name__ == '__main__':
         if xmpp.connect():
             xmpp.process(block=True)
         else:
-            print("Unable to connect.")
+            logger.error('Unable to connect')
     finally:
         # Always close db thread
         db_queue.put('stop')
         db_thread.join()
-    print("Done")
+    logger.info('Done')
